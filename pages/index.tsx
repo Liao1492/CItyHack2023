0133c01ec21c636adbca3dcc5be499ae574f6404
huaxin <liao1492@Huas-MacBook-Air.local>
2023-02-11 14:01:15 +0800

    project setup

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  return <div className="text-[10rem] font-bold">Adil Is Stupid</div>;
+};
+
+export default Home;
